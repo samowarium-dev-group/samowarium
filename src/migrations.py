12a20d2ee21a62fbcf089f1ac0896aee1e574317
Lint Action <lint-action@samuelmeuli.com>
2025-01-04 14:24:51 +0000

    Fix code style issues with Black

--- src/migrations.py
+++ src/migrations.py
@@ -3,6 +3,8 @@ import env
 
 
 def run_postgres_migrations():
-    backend = get_backend(f"postgres://{env.get_postgres_user()}:{env.get_postgres_password()}@{env.get_postgres_host()}/{env.get_postgres_db()}")
+    backend = get_backend(
+        f"postgres://{env.get_postgres_user()}:{env.get_postgres_password()}@{env.get_postgres_host()}/{env.get_postgres_db()}"
+    )
     migrations = read_migrations("./migrations-pg")
     backend.apply_migrations(backend.to_apply(migrations))
